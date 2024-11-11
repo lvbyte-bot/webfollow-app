dac9257a9b7091046805c64c3e2b3e3b5a8029b6
weekend-project-space <sunxzg@gmail.com>
2024-11-11 09:15:39 +0800

    -

--- src/views/item/MagazineItem.vue
+++ src/views/item/MagazineItem.vue
@@ -1,15 +1,25 @@
 <template>
-  <v-list-item class="magazine-item">
-    <div class="magazine py-2">
+  <v-list-item class="magazine-item py-3">
+    <div class="magazine">
       <v-icon :color="item.isRead ? 'grey' : 'primary'">
         {{ item.isRead ? "" : "mdi-circle-medium" }}
       </v-icon>
 
       <div class="desc">
+        <div class="text-body-2 text-medium-emphasis mb-2">
+          <router-link :to="'/f/' + item.feedId" @click.stop="">
+            {{ item.feed?.title }}</router-link
+          >
+        </div>
         <p class="text-truncate mb-2 title">{{ item.title }}</p>
         <p class="text-body-2 text-medium-emphasis">{{ item.summary }}</p>
       </div>
-      <v-img :src="item.thumbnail" cover height="80px"></v-img>
+      <div>
+        <div class="text-body-2 text-medium-emphasis mb-2 text-right">
+          {{ item.datestr }}
+        </div>
+        <v-img :src="item.thumbnail" cover height="80px"></v-img>
+      </div>
     </div>
   </v-list-item>
 </template>
@@ -32,12 +42,22 @@ defineProps(["item"]);
     border-radius: 0.5rem;
   }
 }
+.text-right {
+  text-align: right;
+}
 .desc {
   display: grid;
   width: 100%;
   align-self: flex-start;
   .title {
     font-weight: bold;
+  }
+}
+a {
+  text-decoration: none;
+  color: rgb(var(--v-theme-surface-variant));
+  &:hover {
+    text-decoration: underline;
   }
 }
 </style>
